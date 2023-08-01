0f7a1fbcf3c8617856e08b786cf91f6477e819a1
calllumwhhui <huicallum@gmail.com>
2023-08-01 13:41:35 +0100

    add details page and admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Finch(models.Model):
+    species = models.CharField()
+    scientific_name = models.CharField()
+    habitat = models.CharField()
+    status = models.CharField()
+
+    def __str__(self):
+        return self.species
--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,28 @@
 from django.shortcuts import render
 
-finches = [
-    {
-        'species': 'Zebra Finch',
-        'scientific_name': 'Taeniopygia guttata',
-        'habitat': 'Grasslands and savannas',
-        'status': 'Least Concern',
-    },
-    {
-        'species': 'Gouldian Finch',
-        'scientific_name': 'Erythrura gouldiae',
-        'habitat': 'Tropical savannas',
-        'status': 'Near Threatened',
-    },
-    {
-        'species': 'European Goldfinch',
-        'scientific_name': 'Carduelis carduelis',
-        'habitat': 'Open woodlands and gardens',
-        'status': 'Least Concern',
-    },
-    # Add more finch dictionaries as needed
-]
+from .models import Finch
+
+# finches = [
+#     {
+#         'species': 'Zebra Finch',
+#         'scientific_name': 'Taeniopygia guttata',
+#         'habitat': 'Grasslands and savannas',
+#         'status': 'Least Concern',
+#     },
+#     {
+#         'species': 'Gouldian Finch',
+#         'scientific_name': 'Erythrura gouldiae',
+#         'habitat': 'Tropical savannas',
+#         'status': 'Near Threatened',
+#     },
+#     {
+#         'species': 'European Goldfinch',
+#         'scientific_name': 'Carduelis carduelis',
+#         'habitat': 'Open woodlands and gardens',
+#         'status': 'Least Concern',
+#     },
+#     # Add more finch dictionaries as needed
+# ]
 
 # Create your views here.
 
@@ -31,6 +33,13 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html',{
         'finches': finches
+    })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
     })
