eb32d90fb43e82799ec944b046522f2815b581a7
calllumwhhui <huicallum@gmail.com>
2023-08-03 09:02:56 +0100

    add feeding

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+
+FED = (
+    ('Y', 'Yes'),
+    ('N', 'No')
+)
 # Create your models here.
 
 class Finch(models.Model):
@@ -14,3 +19,20 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+
+class Feeding(models.Model):
+    date = models.DateField('Date Fed')
+    fed = models.CharField(
+        max_length=1,
+        choices=FED,
+        default=FED[1][1]
+        )
+    
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_fed_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ['-date']
